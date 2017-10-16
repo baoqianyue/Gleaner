fd8058d7a3e3a21cd01f6d9a0b74949a913feb63
a6100890 <499531245@qq.com>
2017-10-16 16:18:07 +0800

    配置好了环信sdk

--- app/src/main/java/com/example/a6100890/gleaner/BaseActivity.java
+++ app/src/main/java/com/example/a6100890/gleaner/BaseActivity.java
@@ -1,5 +1,7 @@
 package com.example.a6100890.gleaner;
 
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,7 +14,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class BaseActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class BaseActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private BottomNavigationView mBottomNavigationView;
     private ViewPager mViewPager;
     private MenuItem mMenuItem;
@@ -28,8 +36,10 @@ public class BaseActivity extends AppCompatActivity implements BottomNavigationV
 
         //Bmob应用ID: e4623d0c00213bb18860e466c83b5791
 //        Bmob.initialize(this,"e4623d0c00213bb18860e466c83b5791");
+        //环信 1129171011178759#gleaner
 
         initView();
+        initEasemob();
     }
 
     @Override
@@ -38,7 +48,6 @@ public class BaseActivity extends AppCompatActivity implements BottomNavigationV
         Log.d(TAG, "onCreateOptionsMenu: ");
         return true;
     }
-
 
 
     private void initView() {
@@ -75,11 +84,8 @@ public class BaseActivity extends AppCompatActivity implements BottomNavigationV
         setupViewPager();
 
         setSupportActionBar(mToolbar);
-        mToolbar.setNavigationIcon(R.mipmap.ic_menu);
+        mToolbar.setNavigationIcon(R.mipmap.littlemenu);
 
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_list);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
@@ -89,6 +95,55 @@ public class BaseActivity extends AppCompatActivity implements BottomNavigationV
         adapter.addFragment(LostListFragment.newInstance());
         adapter.addFragment(MessageFragment.newInsatnce());
         mViewPager.setAdapter(adapter);
+    }
+
+    /**
+     * 初始化环信框架
+     */
+    private void initEasemob() {
+        EMOptions options = new EMOptions();
+//        默认添加好友认证
+//        options.setAcceptInvitationAlways(false);
+
+        EMClient.getInstance().init(getApplicationContext(), options);
+        //在做打包混淆时关闭debug模式
+        EMClient.getInstance().setDebugMode(true);
+
+        //如果有其他服务启动活动,保证此初始化模块只进行一次
+        int pid = android.os.Process.myPid();
+        String processAppName = getAppName(pid);
+        if (processAppName == null || !processAppName.equalsIgnoreCase(getPackageName())) {
+            Log.d(TAG, "enter the service process!");
+            return;
+        }
+    }
+
+    /**
+     * 获取正在运行的appName
+     *
+     * @param pID
+     * @return
+     */
+    private String getAppName(int pID) {
+        String processName = null;
+        ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        List list = am.getRunningAppProcesses();
+        Iterator i = list.iterator();
+        PackageManager pm = getPackageManager();
+
+        //对 am.getRunningAppProcesses() 获得的list遍历
+        while (i.hasNext()) {
+            ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo) i.next();
+            try {
+                if (info.pid == pID) {
+                    processName = info.processName;
+                    return processName;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return processName;
     }
 
     @Override
@@ -110,6 +165,7 @@ public class BaseActivity extends AppCompatActivity implements BottomNavigationV
 
     /**
      * 菜单项的点击事件处理
+     *
      * @param item
      * @return
      */
@@ -119,7 +175,6 @@ public class BaseActivity extends AppCompatActivity implements BottomNavigationV
             case R.id.toolbar_user:
                 Log.d(TAG, "onOptionsItemSelected: user icon");
                 break;
-
             case android.R.id.home:
                 Log.d(TAG, "onOptionsItemSelected: android.R.id.home");
             default:
